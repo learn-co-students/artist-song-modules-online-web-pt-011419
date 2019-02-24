9ba7ff5f3ddb0620d30e73b355595eb4c16b8522
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-24 13:49:49 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   
-  extend lib/concerns/memorable.rb 
+  extend module Memorable 
   
   attr_accessor :name
   attr_reader :songs
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+extend module Memorable
+
 class Song
   attr_accessor :name
   attr_reader :artist
